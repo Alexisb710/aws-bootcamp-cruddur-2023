dde5ac2b12ded66fb4e9c91d69b0b1a299a979da
Alexisb710 <alexisbm710@gmail.com>
2024-06-14 11:18:36 -0700

    implement cloudwatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,3 +1,4 @@
+from syslog import LOG_ERR
 from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
@@ -24,10 +25,22 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor, ConsoleSpanExporter
 
 # X-RAY ----------
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+# from aws_xray_sdk.core import xray_recorder
+# from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# CloudWatch Logs
+# import watchtower
+# import logging
+# from time import strftime
 
+# Configuring Logger to Use CloudWatch
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# logger.addHandler(console_handler)
+# logger.addHandler(cw_handler)
+# logger.info("test log")
 
 # Honeycomb ----------------
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -36,8 +49,8 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
 # X-RAY ----------
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+# xray_url = os.getenv("AWS_XRAY_URL")
+# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 
 # show this in the logs within the backend-flask app
@@ -50,7 +63,7 @@ tracer = trace.get_tracer(__name__)
 app = Flask(__name__)
 
 # X-RAY ----------
-XRayMiddleware(app, xray_recorder)
+# XRayMiddleware(app, xray_recorder)
 
 # Honeycomb ----------------
 # Initialize automatic instrumentation with Flask
@@ -67,6 +80,13 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+# Cloudwatch logs
+# @app.after_request
+# def after_request(response):
+#     timestamp = strftime('[%Y-%b-%d %H:%M]')
+#     logger.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#     return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,27 +3,27 @@ from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
   def run(user_handle):
-    with xray_recorder.in_segment('user-activities') as segment:
-      model = {
-        'errors': None,
-        'data': None
-      }
+    # with xray_recorder.in_segment('user-activities') as segment:
+    model = {
+      'errors': None,
+      'data': None
+    }
 
-      now = datetime.now(timezone.utc).astimezone()
-      dict = {
-        "now": now.isoformat()
-      }
-      segment.put_metadata('key', dict, 'namespace')
-      if user_handle == None or len(user_handle) < 1:
-        model['errors'] = ['blank_user_handle']
-      else:
-        now = datetime.now()
-        results = [{
-          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Alexis Bustos',
-          'message': 'Cloud is fun!',
-          'created_at': (now - timedelta(days=1)).isoformat(),
-          'expires_at': (now + timedelta(days=31)).isoformat()
-        }]
-        model['data'] = results
-      return model
+    now = datetime.now(timezone.utc).astimezone()
+    # dict = {
+    #   "now": now.isoformat()
+    # }
+    # segment.put_metadata('key', dict, 'namespace')
+    if user_handle == None or len(user_handle) < 1:
+      model['errors'] = ['blank_user_handle']
+    else:
+      now = datetime.now()
+      results = [{
+        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+        'handle':  'Alexis Bustos',
+        'message': 'Cloud is fun!',
+        'created_at': (now - timedelta(days=1)).isoformat(),
+        'expires_at': (now + timedelta(days=31)).isoformat()
+      }]
+      model['data'] = results
+    return model
