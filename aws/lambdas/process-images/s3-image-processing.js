27acfda415d1fa701c7ef5a5e9d86659fc2fdcda
Alexisb710 <alexisbm710@gmail.com>
2024-07-02 23:37:11 -0700

    implement serverless pipeline

---
+++ aws/lambdas/process-images/s3-image-processing.js
@@ -0,0 +1,57 @@
+import sharp from "sharp";
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+
+function getClient() {
+  const client = new S3Client();
+  return client;
+}
+
+async function getOriginalImage(client, srcBucket, srcKey) {
+  console.log("get==");
+  const params = {
+    Bucket: srcBucket,
+    Key: srcKey,
+  };
+  console.log("params", params);
+  const command = new GetObjectCommand(params);
+  const response = await client.send(command);
+
+  const chunks = [];
+  for await (const chunk of response.Body) {
+    chunks.push(chunk);
+  }
+  const buffer = Buffer.concat(chunks);
+  return buffer;
+}
+
+async function processImage(image, width, height) {
+  const processedImage = await sharp(image)
+    .resize(width, height)
+    .jpeg()
+    .toBuffer();
+  return processedImage;
+}
+
+async function uploadProcessedImage(client, dstBucket, dstKey, image) {
+  console.log("upload==");
+  const params = {
+    Bucket: dstBucket,
+    Key: dstKey,
+    Body: image,
+    ContentType: "image/jpeg",
+  };
+  console.log("params", params);
+  const command = new PutObjectCommand(params);
+  const response = await client.send(command);
+  console.log("repsonse", response);
+  return response;
+}
+
+export const getClient = getClient;
+export const getOriginalImage = getOriginalImage;
+export const processImage = processImage;
+export const uploadProcessedImage = uploadProcessedImage;
