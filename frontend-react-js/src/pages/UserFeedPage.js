9b0d07e6371ca5265a6bba2d4a1e6278adc49d3c
Alexisb710 <alexisbm710@gmail.com>
2024-07-05 15:13:12 -0700

    implement migrations, implement the profile page, implement the endpoint to update profile

--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -6,6 +6,7 @@ import DesktopNavigation from "../components/DesktopNavigation";
 import DesktopSidebar from "../components/DesktopSidebar";
 import ActivityFeed from "../components/ActivityFeed";
 import ActivityForm from "../components/ActivityForm";
+import ProfileForm from "../components/ProfileForm";
 
 import { checkAuth, getAccessToken } from "../lib/CheckAuth";
 import ProfileHeading from "../components/ProfileHeading";
@@ -57,6 +58,11 @@ export default function UserFeedPage() {
       <DesktopNavigation user={user} active={"profile"} setPopped={setPopped} />
       <div className="content">
         <ActivityForm popped={popped} setActivities={setActivities} />
+        <ProfileForm
+          profile={profile}
+          popped={poppedProfile}
+          setPopped={setPoppedProfile}
+        />
         <div className="activity_feed">
           <ProfileHeading setPopped={setPoppedProfile} profile={profile} />
           <ActivityFeed activities={activities} />
