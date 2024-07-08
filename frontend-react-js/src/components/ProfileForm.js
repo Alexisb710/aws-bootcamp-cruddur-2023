a58cac576b9b8d041d996a426f0ba5d210c7c740
Alexisb710 <alexisbm710@gmail.com>
2024-07-08 15:25:31 -0700

    almost able to get avatar uploading working

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -2,6 +2,7 @@ import "./ProfileForm.css";
 import React from "react";
 import process from "process";
 import { getAccessToken } from "lib/CheckAuth";
+import { upload } from "@testing-library/user-event/dist/upload";
 
 export default function ProfileForm(props) {
   const [bio, setBio] = React.useState(0);
@@ -12,6 +13,65 @@ export default function ProfileForm(props) {
     setBio(props.profile.bio);
     setDisplayName(props.profile.display_name);
   }, [props.profile]);
+
+  const s3uploadkey = async (extension) => {
+    console.log("ext", extension);
+    try {
+      const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`;
+      await getAccessToken();
+      const access_token = localStorage.getItem("access_token");
+      const json = {
+        extension: extension,
+      };
+      const res = await fetch(gateway_url, {
+        method: "POST",
+        body: JSON.stringify(json),
+        headers: {
+          Origin: process.env.REACT_APP_FRONTEND_URL,
+          Authorization: `Bearer ${access_token}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      let data = await res.json();
+      if (res.status === 200) {
+        return data.url;
+      } else {
+        console.log(res);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const s3upload = async (event) => {
+    console.log("event", event);
+    const file = event.target.files[0];
+    const filename = file.name;
+    const size = file.size;
+    const type = file.type;
+    const preview_image_url = URL.createObjectURL(file);
+    console.log(filename, size, type);
+    const fileparts = filename.split(".");
+    const extension = fileparts[fileparts.length - 1];
+    const presignedurl = await s3uploadkey(extension);
+    try {
+      console.log("s3upload");
+      const res = await fetch(presignedurl, {
+        method: "PUT",
+        body: file,
+        headers: {
+          "Content-Type": type,
+        },
+      });
+      if (res.status === 200) {
+      } else {
+        console.log(res);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const onsubmit = async (event) => {
     event.preventDefault();
@@ -62,13 +122,16 @@ export default function ProfileForm(props) {
     return (
       <div className="popup_form_wrap profile_popup" onClick={close}>
         <form className="profile_form popup_form" onSubmit={onsubmit}>
-          <div class="popup_heading">
-            <div class="popup_title">Edit Profile</div>
+          <div className="popup_heading">
+            <div className="popup_title">Edit Profile</div>
             <div className="submit">
               <button type="submit">Save</button>
             </div>
           </div>
           <div className="popup_content">
+            <div className="upload" onClick={s3upload}>
+              Upload Avatar
+            </div>
             <div className="field display_name">
               <label>Display Name</label>
               <input
