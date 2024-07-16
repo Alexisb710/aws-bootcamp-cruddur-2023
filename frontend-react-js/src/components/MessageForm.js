680f83d028db2ae82b2da58177ee5880eb09c96e
Alexisb710 <alexisbm710@gmail.com>
2024-07-16 12:53:02 -0700

    minor update

--- frontend-react-js/src/components/MessageForm.js
+++ frontend-react-js/src/components/MessageForm.js
@@ -1,7 +1,7 @@
 import "./MessageForm.css";
 import React from "react";
 import process from "process";
-import { useParams } from "react-router-dom";
+import { json, useParams } from "react-router-dom";
 import { getAccessToken } from "lib/CheckAuth";
 
 export default function ActivityForm(props) {
