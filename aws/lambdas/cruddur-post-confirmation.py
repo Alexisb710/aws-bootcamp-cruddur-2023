f33f166d3cc55e7613f896a658780e1f4537e627
Alexisb710 <alexisbm710@gmail.com>
2024-07-21 18:56:28 -0700

    fixed lambda error, updated service script with parameters, and minor fixes to code

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -10,9 +10,11 @@ def lambda_handler(event, context):
     user_display_name = user['name']
     user_email        = user['email']
     user_handle       = user['preferred_username']
-    user_cognito_id   = user['sub']
+    cognito_user_id   = user['sub']
+
     try:
       print('entered-try')
+
       sql = f"""
         INSERT INTO public.users (
           display_name,
@@ -20,19 +22,24 @@ def lambda_handler(event, context):
           handle, 
           cognito_user_id
           )
-        VALUES(%s,%s,%s,%s) 
+        VALUES(
+          %(display_name)s,
+          %(email)s,
+          %(handle)s,
+          %(cognito_user_id)s
+        ) 
       """
       print("SQL STATEMENT -----------")
       print(sql)
       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       cur = conn.cursor()
-      params = [
-        user_display_name,
-        user_email,
-        user_handle,
-        user_cognito_id
-      ]
-      cur.execute(sql,*params)
+      params = {
+        'display_name': user_display_name,
+        'email': user_email,
+        'handle': user_handle,
+        'cognito_user_id': cognito_user_id
+      }
+      cur.execute(sql,params)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
@@ -42,4 +49,5 @@ def lambda_handler(event, context):
         cur.close()
         conn.close()
         print('Database connection closed.')
+
     return event
