34eac9ce7bc74373409945f3afb9bdf98d8bbc35
Alexisb710 <alexisbm710@gmail.com>
2024-07-21 22:05:49 -0700

    syntax error fixes and package installs

--- frontend-react-js/src/components/DesktopNavigationLink.js
+++ frontend-react-js/src/components/DesktopNavigationLink.js
@@ -4,8 +4,16 @@ import { ReactComponent as NotificationsIcon } from "./svg/notifications.svg";
 import { ReactComponent as ProfileIcon } from "./svg/profile.svg";
 import { ReactComponent as MoreIcon } from "./svg/more.svg";
 import { ReactComponent as MessagesIcon } from "./svg/messages.svg";
+import { useParams } from "react-router-dom";
 
 export default function DesktopNavigationLink(props) {
+  const params = useParams();
+  console.log("start [error]::");
+  console.log("this is the params.handle value: ", params.handle);
+  console.log("end [error]::");
+  const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/@${params.handle}`;
+  console.log("this is the backend_url value: ", backend_url);
+
   const classes = () => {
     const classes = ["primary"];
     if (props.handle === props.active) {
@@ -18,24 +26,23 @@ export default function DesktopNavigationLink(props) {
     switch (props.handle) {
       case "home":
         return <HomeIcon className="icon" />;
-        break;
       case "notifications":
         return <NotificationsIcon className="icon" />;
-        break;
       case "profile":
         return <ProfileIcon className="icon" />;
-        break;
       case "more":
         return <MoreIcon className="icon" />;
-        break;
       case "messages":
         return <MessagesIcon className="icon" />;
-        break;
       default:
-        break;
+        return null; // Optionally handle default case
     }
   };
 
+  console.log("start [error]::");
+  console.log("this is the props.url value: ", props.url);
+  console.log("this is the props.name value: ", props.name);
+  console.log("end [error]::");
   return (
     <Link to={props.url} className={classes()} href="#">
       {icon()}
